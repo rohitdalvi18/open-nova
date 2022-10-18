82d4b58b3d89c20914202cd0288886a9ac93515e
Rohit <rohit.dalvi2001@gmail.com>
2022-10-18 13:07:30 +0530

    tweet component complete #7

---
+++ components/Tweet.tsx
@@ -0,0 +1,54 @@
+const Tweet = () => {
+
+    let circumference = 60
+    let percent = 75
+
+    return (
+        <div className="w-3/5 flex flex-row">
+            <div className="flex flex-col items-center">
+                <div className="w-14 rounded-full mb-2">
+                    <img className="rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCdyi-jV5u0bVQC1BIH0LAH2wJT_p56qUNQ&usqp=CAU" alt="avatar" />
+                </div>
+                <div className="w-0.5 h-10 thread-link"></div>
+            </div>
+            <div className="w-full ml-3 flex flex-col">
+                <div className="flex items-center justify-between">
+                    <p>
+                        <span className="theme-text font-semibold">Paul Hutton</span>&nbsp;
+                        <span className="theme-text-accent font-regular">@paulcito07</span>
+                    </p>
+                    <div className="rounded-full">
+                        <svg className="w-8 h-8">
+                            <circle
+                            className="text-gray-300"
+                            stroke-width="5"
+                            stroke="currentColor"
+                            fill="transparent"
+                            r="10"
+                            cx="14"
+                            cy="14"
+                            />
+                            <circle
+                            className="stroke-cyan-500"
+                            strokeWidth="5"
+                            strokeDasharray={circumference}
+                            strokeDashoffset={circumference - percent / 100 * circumference}
+                            strokeLinecap="round"
+                            // stroke="stroke:text-blue-600"
+                            fill="transparent"
+                            r="10"
+                            cx="14"
+                            cy="14"
+                            />
+                        </svg>
+                    </div>
+                </div>
+                <div className="mt-1 w-full">
+                    <input className=" w-full bg-transparent" type="text" name="tweet" placeholder="What's happening?" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tweet;
--- pages/user/new-tweet.tsx
+++ pages/user/new-tweet.tsx
@@ -1,9 +1,15 @@
 import WithNavbar from "../../components/WithNavbar"
+import Tweet from "../../components/Tweet"
 
 const NewTweet = ()=>{
     return(
         <WithNavbar>
-            New Tweets
+            <div className="w-4/5 px-5 my-4">
+                <h4 className="theme-text text-2xl font-semibold">New Tweet</h4>
+                <div className="flex flex-col items-center content-start mt-5">
+                    <Tweet/>
+                </div>
+            </div>
         </WithNavbar>
     )
 }
